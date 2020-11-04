3ec5ca337d73e618958d2d8531712c9c50532927
chenjian <chenjian@zhehekeji.com>
2020-11-04 18:00:17 +0800

    打包apk配置

--- index.js
+++ index.js
@@ -3,8 +3,12 @@
  */
 
 import {AppRegistry} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import 'react-native-gesture-handler';
 import App from './js/App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => {
+    SplashScreen.hide();
+    return App
+});
--- js/pages/WebviewProductScreen.js
+++ js/pages/WebviewProductScreen.js
@@ -1,24 +1,28 @@
 import React from 'react';
 import {StyleSheet, Text, Button, View} from 'react-native';
 import {WebView} from 'react-native-webview';
-import BackPressComponent from '../common/BackPressComponent';
 
 export default class WebViewPage extends React.Component {
-   
+    constructor(props) {
+        super(props);
+        this.state = {title: '11'}
+    }
+    onNavigationStateChange(navState) {
+        console.log(navState)
+        this.setState({
+            title: navState.title
+        });
+    }
+
     render() {
         return (
-            <View>
-                {/* <View>
-                    <Button
-                        title="goback"
-                        onPress={() => this.onBackPress()}
-                    />
-                    <Text>{this.state.title}</Text>
-                </View> */}
+            <>
+                <Text>{this.state.title}</Text>
                 <WebView
-                    source={{uri: 'https://reactnative.dev/'}}
+                    source={{uri: 'http://lxfq.smqr.net/login_'}}
+                    onNavigationStateChange={e => this.onNavigationStateChange(e)}
                 />
-            </View>
+            </>
         );
     }
 }
